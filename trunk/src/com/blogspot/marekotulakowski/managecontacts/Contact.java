f42bb78141bba139f84caf56d9c33a33095d4811
marekotulakowski@gmail.com <marekotulakowski@gmail.com@f2de1d62-375b-805b-7de4-19e61c5ee4f9>
2012-05-06 17:55:56 +0000

    v.1.0.0.0 (build 007)

---
+++ trunk/src/com/blogspot/marekotulakowski/managecontacts/Contact.java
@@ -0,0 +1,47 @@
+/*
+ * build 007
+ */
+package com.blogspot.marekotulakowski.managecontacts;
+
+public class Contact {  
+   
+	 //Variables  
+	 private String name;  
+	 private String phoneNumber;  
+	 private TypeContact typeContact;  
+	   
+	 public enum TypeContact { 
+		 ContactFromSIMcard,  
+		 ContactFromAndroid  
+	 };  
+	   
+	 public String getName() {  
+		 return name;  
+	 }  
+	   
+	 public String getPhoneNumber() {  
+		 return phoneNumber;  
+	 }  
+	   
+	 public TypeContact getTypeContact() {  
+		 return typeContact;  
+	 }  
+	   
+	 public void setName(String _name) {  
+		 name = _name;  
+	 }  
+	   
+	 public void setPhoneNumber(String _telephone) {  
+		 phoneNumber = _telephone;  
+	 }  
+	   
+	 public void setTypeContact(TypeContact _typeContact) {  
+		 typeContact = _typeContact;  
+	 }  
+	   
+	 Contact(String _name, String _telephone, TypeContact _typeContact) {  
+		  name = _name;  
+		  phoneNumber = _telephone;  
+		  typeContact = _typeContact;  
+	 }   
+}  
---
+++ trunk/src/com/blogspot/marekotulakowski/managecontacts/SourceContact.java
@@ -0,0 +1,10 @@
+/*
+ * build 007
+ */
+package com.blogspot.marekotulakowski.managecontacts;
+
+public enum SourceContact {  
+	 SIMcardContact,  
+	 AndroidContact,  
+	 SIMandAndroid  
+}  
